ea9408c92e0a2884df3beb4993b7df0be0cf304c
a-sepulveda-r <ariel.sepulvedar@gmail.com>
2023-06-12 14:47:26 -0400

    feat: CounterAPP Component

--- src/01-useState/CounterApp.jsx
+++ src/01-useState/CounterApp.jsx
@@ -1,5 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const CounterApp = () => {
-  return <div>CounterApp</div>;
+  //   const [counter, setCounter] = useState({
+  //     counter1: 10,
+  //     counter2: 20,
+  //     counter3: 30,
+  //   });
+  const [state, setCounter] = useState({
+    counter1: 10,
+    counter2: 20,
+    counter3: 30,
+  });
+
+  const { counter1, counter2, counter3 } = state;
+  return (
+    <>
+      <h1>{counter1}</h1>
+      <h1>{counter2}</h1>
+      <h1>{counter3}</h1>
+      <hr />
+      <button
+        className="btn btn-primary"
+        onClick={() => {
+          setCounter({
+            ...state,
+            counter1: counter1 + 1,
+            counter2: counter2 - 1,
+          });
+        }}
+      >
+        +1
+      </button>
+    </>
+  );
 };
